5aba7338abe8cd04f030c71e66d85250ae90aeb0
BoyanAndonov <andonov_boyan@yahoo.com>
2024-02-02 09:13:19 +0200

    Create pop.js

---
+++ pop.js
@@ -0,0 +1,15 @@
+function solve()
+{
+
+     let array =[1,2,3,4,5];
+
+     console. log("Array: "+ array);
+
+     //pop operation
+     console. log("What Pop() method is returning: "+ array.pop());
+     console. log("Array after pop: " + array);
+     console. log("Array length after pop:" + array. length);
+
+
+}
+solve();
