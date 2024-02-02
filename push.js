ce1695ae25381ae9e07e6f94f9c176016806c587
BoyanAndonov <andonov_boyan@yahoo.com>
2024-02-02 09:13:22 +0200

    Create push.js

---
+++ push.js
@@ -0,0 +1,11 @@
+function solve(){
+
+    let array =[1,2,3,4,5];
+    //push operation
+    
+    console.log("What push() is returning: " + array. push (10));
+    console. log("Array after push: " + array);
+    console. log("Array length after push:" + array. length);
+
+}
+solve();
